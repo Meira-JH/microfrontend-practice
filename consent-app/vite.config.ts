2434913d85acccbcbd65eae5b706cec333d2be05
Meira-JH <jhalvesmeira@gmail.com>
2025-09-27 14:44:45 -0300

    feat: TASK-1 consent_app served through host_app

--- consent-app/vite.config.ts
+++ consent-app/vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import federation from "@originjs/vite-plugin-federation";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    federation({
+      name: "consent_app",
+      filename: "remoteConsentContainer.js",
+      exposes: {
+        "./ConsentManager": "./src/components/ConsentManager.tsx",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
+  server: {
+    port: 3001,
+    cors: {
+      origin: "*",
+    },
+  },
+});
